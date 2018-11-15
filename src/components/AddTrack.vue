c3ae6718c41fddf97614b0391316a13a06262455
Bernard Cooley <bernardcooley@gmail.com>
2018-11-15 23:19:00 +0000

    Track upload complete

--- src/components/AddTrack.vue
+++ src/components/AddTrack.vue
@@ -2,40 +2,45 @@
   <div class="addTrackContainer">
     <div class="content">
       <div class="pageContainer">
-        <q-list v-if="!fileUploadingComp">
+        <q-list v-if="!uploadingFile">
           <q-item>
-            <q-field label="Artist" error-label="">
+            <q-field label="Artist">
               <q-input id="artist" v-model="track.artist.value" />
+              <div class="validationMessage">{{track.artist.errorMessage}}</div>
             </q-field>
           </q-item>
 
           <q-item>
-            <q-field label="Title" error-label="">
+            <q-field label="Title">
               <q-input id="trackTitle" v-model="track.title.value" />
+              <div class="validationMessage">{{track.title.errorMessage}}</div>
             </q-field>
           </q-item>
 
           <q-item>
-            <q-field label="Artwork Url (optional)" error-label="">
+            <q-field label="Artwork Url (optional)">
               <q-input id="artworkUrl" v-model="track.artworkUrl.value" />
             </q-field>
           </q-item>
 
           <q-item>
-            <q-field label="Upload Track" error-label="">
+            <q-field label="Upload Track">
               <input type="file" ref="trackUpload" multiple @change="getUploadFile" class="input-file">
             </q-field>
           </q-item>
 
-          
         </q-list>
         <q-item>
-            <!--<q-progress :percentage="updatePercentage" />-->
-            <div v-if="fileUploadingComp">{{fileUploadPercentage}}% uploaded</div>
-          </q-item>
+          <!--<q-progress :percentage="updatePercentage" />-->
+          <div v-if="uploadingFile && !completedUpload">{{fileUploadPercentage}}% uploaded</div>
+        </q-item>
 
-          <q-btn v-if="!fileUploadingComp" v-on:click="uploadFile(fileToUpload)">Upload</q-btn>
-          <q-btn v-if="fileUploadingComp" v-on:click="cancel">Cancel</q-btn>
+        <q-btn v-if="!uploadingFile" v-on:click="uploadFile(fileToUpload)">Upload</q-btn>
+        <q-btn v-if="uploadingFile && !completedUpload" v-on:click="cancelUpload">Cancel</q-btn>
+        <div class="uploadCompleteContainer" v-if="uploadComplete">
+          <div class="uploadSuccessMessage">Upload Complete</div>
+          <q-btn v-on:click="resetForm">Upload Another Track</q-btn>
+        </div>
       </div>
     </div>
   </div>
@@ -57,51 +62,49 @@ export default {
   data: function() {
     return {
       track: {
-        artist: { value: null, errors: [] },
-        title: { value: null, errors: [] },
-        trackUrl: { value: null, errors: [] },
-        artworkUrl: { value: null, errors: [] },
-        uploadedBy: { value: null, errors: [] }
+        artist: { value: null, errorMessage: '' },
+        title: { value: null, errorMessage: '' },
+        artworkUrl: { value: null },
+        uploadedBy: { value: null }
       },
-      errorsBool: null,
       userID: null,
       addTrackMessage: null,
       trackUpload: null,
       fileToUpload: null,
       uploadTask: null,
-      fileUploading: false
+      fileUploading: false,
+      completedUpload: false
     };
   },
   computed: {
     ...mapState(["fileUploadPercentage"]),
-    fileUploadingComp() {
+    uploadingFile() {
       return this.fileUploading
+    },
+    uploadComplete() {
+      return this.completedUpload
     }
   },
   methods: {
     ...mapMutations(['UPDATE_ADD_TRACK', 'UPDATE_FILE_UPLOAD_PERCENTAGE']),
     validation: function(e) {
-      this.errorsBool = false;
-      this.track.artist.errors = [];
-      this.track.title.errors = [];
-      this.track.trackUrl.errors = [];
-      this.track.artworkUrl.errors = [];
-
       if (!this.track.artist.value) {
-        this.track.artist.errors.push("Artist name required.");
+        this.track.artist.errorMessage = 'Artist is required.'
+      }else {
+        this.track.artist.errorMessage = ''
       }
 
       if (!this.track.title.value) {
-        this.track.title.errors.push("Track title required.");
-      }
-
-      if (!this.track.trackUrl.value) {
-        this.track.trackUrl.errors.push("Track Url required.");
+        this.track.title.errorMessage = 'Title is required.'
+      }else {
+        this.track.title.errorMessage = ''
       }
 
       for (var x in this.track) {
-        if (this.track[x].errors.length > 0) {
-          this.errorsBool = true;
+        if (this.track[x].errorMessage.length > 0) {
+          return true
+        } else {
+          return false
         }
       }
     },
@@ -120,56 +123,92 @@ export default {
     //     // Uh-oh, an error occurred!
     //   });
     // },
+    doesFileExist: function() {
+      firebase.storage().ref().child('tracks/' + this.fileToUpload.name).getDownloadURL().then(function() {
+        return true
+      }).catch(function() {
+        return false
+      })
+    },
     uploadFile: function(fileToUpload) {
+      let self = this
+      this.fileToUpload = fileToUpload
       let store = this.$store
-      this.validation();
-      // if (!this.errorsBool) {
-      this.fileUploading = true
-      let storageRef = firebase.storage().ref()
-      let tracksRef = storageRef.child('tracks')
-      let focusedTrack = tracksRef.child(fileToUpload.name)
 
-      var metadata = {
-        customMetadata: {
-          'artist': this.track.artist.value,
-          'title': this.track.title.value,
-          'artworkUrl': this.track.artworkUrl.value,
-          'uploadedBy': firebase.auth().currentUser.uid
+      if (!this.validation()) {
+        if (!this.doesFileExist()) {
+          self.completedUpload = false
+          self.fileUploading = true
+          let storageRef = firebase.storage().ref()
+          let tracksRef = storageRef.child('tracks')
+          let focusedTrack = tracksRef.child(fileToUpload.name)
+
+          var metadata = {
+            customMetadata: {
+              'artist': this.track.artist.value,
+              'title': this.track.title.value,
+              'artworkUrl': this.track.artworkUrl.value,
+              'uploadedBy': firebase.auth().currentUser.uid
+            }
+          }
+
+          this.uploadTask = focusedTrack.put(fileToUpload, metadata)
+
+          this.uploadTask.on('state_changed',
+            function progress(snapshot) {
+              store.commit("UPDATE_FILE_UPLOAD_PERCENTAGE", Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100))
+            },
+            function errors(err) {
+
+            },
+            function complete() {
+              console.log('Upload Complete')
+              self.completedUpload = true
+            }
+          )
         }
       }
-
-      this.uploadTask = focusedTrack.put(fileToUpload, metadata)
-
-      this.uploadTask.on('state_changed',
-        function progress(snapshot) {
-          store.commit("UPDATE_FILE_UPLOAD_PERCENTAGE", Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100))
-        },
-        function errors(err) {
-
-        },
-        function complete() {
-          console.log('Upload Complete')
-          this.fileUploading = false
-        }
-      )
-      // }
     },
-    cancel: function() {
+    cancelUpload: function() {
       this.uploadTask.cancel()
+      this.resetForm()
+    },
+    resetForm: function() {
       this.fileUploading = false
+      this.completedUpload = false
+      this.track = {
+        artist: { value: null, errorMessage: '' },
+        title: { value: null, errorMessage: '' },
+        artworkUrl: { value: null },
+        uploadedBy: { value: null }
+      }
     }
   }
 };
 </script>
 
-<style>
+<style lang='scss'>
 .validationMessage {
   color: red;
-  text-align: center;
+  text-align: left;
 }
 
 .native-input:active,
 .native-input:focus {
   outline: none;
+}
+
+.uploadCompleteContainer {
+  display: flex;
+  font-size: 20px;
+  padding-top: 50px;
+  flex-direction: column;
+  align-items: center;
+
+  .uploadSuccessMessage {
+    margin: 50px 20px;
+    color: green;
+    text-align: center;
+  }
 }
 </style>
