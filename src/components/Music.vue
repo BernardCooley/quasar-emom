fa77ef6ee1b54614d7b2fc52abab823bc1544fcb
Bernard Cooley <bernardcooley@gmail.com>
2018-11-17 01:00:10 +0000

    Click tracks working

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -28,7 +28,7 @@
           <q-list>
             <h3 class="trackListTitle">All tracks</h3>
             <q-item class="row" v-for="(track, index) in tracks" :key="index">
-              <div class="allTracksArtistAndTitle col-11" v-on:click="changeTrack(track.metaData.artist, track.metaData.title)">
+              <div class="allTracksArtistAndTitle col-11" v-on:click="changeTrack(track.filename)">
                 <div class="allTracksArtists">{{track.metaData.artist}}</div>
                 <div class="allTracksTitles">{{track.metaData.title}}</div>
               </div>
@@ -63,25 +63,6 @@ export default {
   },
   methods: {
     ...mapMutations(['UPDATE_CURRENT_TRACK', 'UPDATE_TRACK_LIST', 'UPDATE_TRACKS_ARRAY', 'CLEAR_TRACKS_ARRAY']),
-    loadTracks: function() {
-      this.tracks = [];
-      db.collection("tracks").get()
-        .then(querySnapshot => {
-          var index = 0
-          querySnapshot.forEach(doc => {
-            var data = doc.data()
-            data['trackIndex'] = index
-            this.tracks.push(data)
-            index++
-          });
-          this.$store.commit("UPDATE_TRACK_LIST", this.tracks)
-          if (this.tracks) {
-            this.dataLoaded = true;
-          }
-          this.$store.commit('UPDATE_CURRENT_TRACK', this.tracks[this.currentTrackIndexNumber])
-          this.$store.commit('UPDATE_LIKES', this.tracks[this.currentTrackIndexNumber].likes)
-        });
-    },
     getUserTracks: function(metaData) {
       let store = this.$store
       let usersRef = db.collection("users").doc(firebase.auth().currentUser.uid)
@@ -124,53 +105,6 @@ export default {
 
 
     },
-    getTrackUrl: function() {
-      let storageRef = firebase.storage().ref()
-      // Create a reference to the file we want to download
-      var starsRef = storageRef.child('tracks/Logic_-_44_More.mp3');
-
-      // Get the download URL
-      starsRef.getDownloadURL().then(function(url) {
-        this.trackurl = url
-        // Insert url into an <img> tag to "download"
-      }).catch(function(error) {
-
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-          case 'storage/object-not-found':
-            // File doesn't exist
-            break;
-
-          case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break;
-
-          case 'storage/canceled':
-            // User canceled the upload
-            break;
-
-          case 'storage/unknown':
-            // Unknown error occurred, inspect the server response
-            break;
-        }
-      });
-    },
-    getTracks: function() {
-      // let client = this.sdk.getBasicClient('kQ2wdszzI69gnF0U4sYun9FIa0x3wJwu')
-      // client.users.get(client.CURRENT_USER_ID, null, function (err, currentUser) {
-      //   if (err) throw err;
-      //   console.log('Hello, ' + currentUser.name + '!');
-      // });
-      const token = 'Ek7KVd8kjUHSuvIujbGsR1iFN1olULbk'
-      return fetch('https://api.box.com/2.0/folders/0/items', {
-        headers: {
-          Authorization: `token ${token}`
-        }
-      })
-        .then(res => res.json())
-        .then(tracks => alert(tracks))
-    },
     previousTrack: function() {
       if (this.currentTrackIndexNumber > 0) {
         this.currentTrackIndexNumber--
@@ -181,14 +115,16 @@ export default {
         this.currentTrackIndexNumber++
       }
     },
-    changeTrack: function(artist, title) {
-      this.getTrackUrl()
-      this.currentTrackIndexNumber = this.tracks.filter(track => track.title.toLowerCase() == title.toLowerCase() && track.artist.toLowerCase() == artist.toLowerCase())[0].trackIndex
+    changeTrack: function(filename) {
+      this.tracks.forEach((track, index) => {
+        if(track.filename === filename) {
+          this.currentTrackIndexNumber = index
+        }
+      })
       this.$store.commit('UPDATE_CURRENT_TRACK', this.tracks[this.currentTrackIndexNumber])
     }
   },
   created() {
-    // this.loadTracks()
     this.getUserTracks()
   },
   computed: {
@@ -196,19 +132,9 @@ export default {
       return this.tracks.filter(track => track.title.toLowerCase().includes(this.search.toLowerCase()) || track.artist.toLowerCase().includes(this.search.toLowerCase()))
     },
     currentTrack() {
-      // return this.tracks[this.currentTrackIndexNumber]
-      // console.log(this.tracks[0])
-
-      // console.log(this.tracks[0])
-
-      // this.tracks.forEach(track => {
-      //   console.log(track.downloadURL)
-      // })
-
-
-      return this.tracks[0]
+      return this.tracks[this.currentTrackIndexNumber]
     },
-    ...mapState(['trackList', 'tracksArray'])
+    ...mapState(['trackList'])
   }
 };
 </script>
